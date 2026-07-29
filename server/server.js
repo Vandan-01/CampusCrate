9b635d5585ce6ae939ca6199c0ab0bffb71f1ab1
Vandan <0808cs241363.ies@ipsacademy.org>
2026-07-29 21:07:52 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "CampusCrate API Running 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
