4ae1b0e4941a9ff229c7ea721f2c562845fb96f6
Vedant Sharma <vedantsharma9754@gmail.com>
2026-08-01 18:28:31 +0530

    feat(models):add Item,Claim and Report schemas

---
+++ server/models/Claim.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const claimSchema = new mongoose.Schema(
+  {
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
+
+    claimantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Claim", claimSchema);
